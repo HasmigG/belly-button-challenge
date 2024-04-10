10b459847f941ae13d74a88cc37d0727b92fb42e
HasmigG23 <HasmigG23@gmail.com>
2024-04-10 09:58:40 -0700

    update

--- static/js/app.js
+++ static/js/app.js
@@ -9,6 +9,7 @@ async function optionChanged(id) {
     if (id == undefined) {
         id = names[0];
 
+        d3.select('select').html('');
         names.forEach(name => {
             d3.select('select').append('option').text(name);
         });
@@ -17,7 +18,27 @@ async function optionChanged(id) {
     // Demographic Info
     let meta = metadata.find(dict => dict.id == id);
 
+    d3.select('#sample-metadata').html('');
+    Object.entries(meta).forEach(([key, val]) => {
+        d3.select('#sample-metadata').append('h5').html(`<b><i>${key}:</i></b> ${val}`);
+    });
+
+    // Bar Chart
+
+    let {otu_ids, otu_labels, sample_values } = samples.find(dict => dict.id == id);
     
+    // console.log(data1.otu_ids, data1.otu_labels, data1.sample_values);
 
+    console.log(otu_ids);
 
+    var data = [
+        {
+            x: ['giraffes', 'orangutans', 'monkeys'],
+            y: [20, 14, 23],
+            type: 'bar',
+            orientation: 'h'
+        }
+    ];
+
+    Plotly.newPlot('bar', data);
 };
